eae3bad25cd765ddbd31b43cd2d6f289972e388b
Yoko Harada <yokolet@gmail.com>
2019-06-15 16:33:13 -0700

    feat: adds Material Icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 export default () => {
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <a href="/" className="brand-logo">Transcript English to Katakana</a>
-      </div>
-    </nav>
+    <div>
+      <nav>
+        <div className="row">
+          <div className="col s12">
+            <div className="nav-wrapper">
+              <a href="#!" className="brand-logo">Transcript English to Katakana</a>
+              <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+                <i className="material-icons">menu</i>
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="about.html">About</a></li>
+              </ul>
+            </div>
+        </div>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+        <li><a href="about.html">About</a></li>
+      </ul>
+    </div>
   )
 }
